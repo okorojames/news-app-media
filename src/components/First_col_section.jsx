c6cb86747c9c799419af011a400dfacdd3409669
okorojames <okorojameschizaram@gmail.com>
2023-02-07 12:25:58 +0100

    working on the light and dark mode toggle

--- src/components/First_col_section.jsx
+++ src/components/First_col_section.jsx
@@ -2,22 +2,30 @@ import imageOne from "../images/news_5.jpg"
 import imageTwo from '../images/news_6.jpg'
 import imageThree from "../images/news_8.jpg"
 const First_col_section = () => {
+  function modeToggle() {
+    alert("hello")
+  }
   return (
     <div className="three-col-carousel">
       {/*  */}
       <div className="carousel-card">
         <img src={imageOne} alt="" />
-        <div className="carousel-card-context">Lorem ipsum, dolor sit amet consectetur adipisicing <br /> elit. Iure alias animi velit officia. Voluptatibus eligendi quia quos consequatur eaque totam </div>
+        <div className="carousel-card-context">Lorem ipsum, dolor sit amet consectetur adipisicing elit officia.</div>
       </div>
       {/*  */}
       <div className="carousel-card">
         <img src={imageTwo} alt="" />
-        <div className="carousel-card-context">Lorem ipsum, dolor sit amet consectetur adipisicing <br /> elit. Iure alias animi velit officia. Voluptatibus eligendi quia quos consequatur eaque totam </div>
+        <div className="carousel-card-context">Lorem ipsum, dolor sit amet consectetur adipisicing elit officia.</div>
       </div>
       {/*  */}
       <div className="carousel-card">
         <img src={imageThree} alt="" />
-        <div className="carousel-card-context">Lorem ipsum, dolor sit amet consectetur adipisicing <br /> elit. Iure alias animi velit officia. Voluptatibus eligendi quia quos consequatur eaque totam </div>
+        <div className="carousel-card-context">Lorem ipsum, dolor sit amet consectetur adipisicin elit. officia.</div>
+      </div>
+      {/* DARK AND LIGHT MODE */}
+      <div className="darkAndLight">
+        <i className="ri-sun-line lightMode" onClick={modeToggle}></i>
+        <i className="ri-moon-fill darkMode" onClick={modeToggle}></i>
       </div>
     </div>
   )
